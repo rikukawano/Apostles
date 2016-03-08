1edb4446d51098926a0eedf84bf373d072f3e432
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-03-08 11:56:48 +0900

    Set up Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Default URL options for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
