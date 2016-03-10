b6a00cb3c2dbe43d19226940a4bd4596c85fdd31
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-03-10 18:13:08 +0900

    Add Database Relations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ActiveRecord::Base
+	has_one :stat
+	has_many :weight_stat, through: :stat
+	has_many :height_stat, through: :stat
+	has_many :squat_stat, through: :stat
+	has_many :benchpress_stat, through: :stat
 end
