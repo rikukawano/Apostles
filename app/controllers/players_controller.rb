950193df6a1efd83a7b03de44e7a8f5f9c9b4461
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-04-19 16:11:50 +0900

    Add player index page

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,19 @@
+class PlayersController < ApplicationController
+	def index
+		@players = Player.all
+	end
+
+	def new 
+		@player = Player.new
+	end
+	
+	def create
+		@player = Player.create(player_params)
+		redirect_to root_path
+	end
+
+	def player_params
+		params.require(:player).permit(:name, :year, :number)
+	end
+
+end
