ae1b69f823cef62323bd38f9817520c7cf2d504a
rikukawano <Riku@Rikus-MacBook-Pro.local>
2016-04-20 21:08:46 +0900

    Create index page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,6 +12,25 @@ class PlayersController < ApplicationController
 		redirect_to root_path
 	end
 
+	def edit
+		@player = Player.find(params[:id])
+	end
+
+	def update
+		@player = Player.find(params[:id])
+		if @player.update(player_params)
+			redirect_to root_path
+		else
+			redirect_to edit_player_path(params[:id])
+		end
+	end
+
+	def destroy
+		@player = Player.find(params[:id])
+		@player.destroy
+		redirect_to :back
+	end
+
 	def player_params
 		params.require(:player).permit(:name, :year, :number)
 	end
